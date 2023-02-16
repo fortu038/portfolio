ef549fcb69eaf3edf76f3725fe5eb8d846d9cc2e
fortu038 <fortu038@umn.edu>
2023-02-15 19:30:48 -0600

    removed contact form

--- src/components/pages/contact-me/ContactMe.jsx
+++ src/components/pages/contact-me/ContactMe.jsx
@@ -10,16 +10,6 @@ function ContactMe () {
 
       <div className="d-flex flex-column justify-content-center align-items-center">
         <h2>Contact Me</h2>
-        <h6 className="text-center">Note: The below contact form is currently not functional</h6>
-        <form>
-          <label for="name">Your Name:</label><br />
-          <input type="text" id="name" name="name" /><br />
-          <label for="email">Your Email:</label><br />
-          <input type="text" id="email" name="email" /><br />
-          <label for="message">Your Message:</label><br />
-          <input type="text" id="message" name="message" /><br />
-          <button type="button">Submit</button>
-        </form>
         <h5 className="text-center">My Primary Email: <a className="HoverLink" href="mailto:conorfortuna@gmail.com">conorfortuna@gmail.com</a></h5>
         <h5 className="text-center">My Secondary Email: <a className="HoverLink" href="mailto:fortu038@umn.edu">fortu038@umn.edu</a></h5>
         <h5 className="text-center">My Phone Number: <a className="HoverLink" href="tel:2183404672">218-340-4672</a></h5>
